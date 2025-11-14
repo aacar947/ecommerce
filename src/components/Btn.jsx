efd18711091ec3fbf26ca53ab4527e5ceabdacca
Abdullah Acar <aacar0947@gmail.com>
2025-11-14 17:07:28 +0300

    Reorder props in Btn component for improved readability

--- src/components/Btn.jsx
+++ src/components/Btn.jsx
@@ -2,7 +2,7 @@ import Icon from './Icon';
 
 export default function Btn({ children, icon, className, padding, iconSize = '32px', style, ...props }) {
   return (
-    <button className={className ? 'btn ' + className : 'btn'} style={{ padding, ...style }} {...props}>
+    <button {...props} className={className ? 'btn ' + className : 'btn'} style={{ padding, ...style }}>
       {icon ? <Icon icon={icon} size={iconSize} /> : null}
       {children && <span>{children}</span>}
     </button>
